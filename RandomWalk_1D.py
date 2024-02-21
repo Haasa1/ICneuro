7f0c9dba35a08f627efea083925a94fd4cf8a4ed
Haasa1 <matheushaasbr@gmail.com>
2024-02-21 13:12:01 -0300

    modified py file

--- RandomWalk_1D.py
+++ RandomWalk_1D.py
@@ -51,37 +51,60 @@ plt.text(10, 95, f'p = {p} \nx$_0$ = {x0}',va='top',ha='left')
 #plt.yticks(numbersy)
 plt.show()
 
-def Caminhadas_aleatorias_de_max_T(N,x0,p,T_values):
+def Caminhadas_aleatorias_de_max_T(N,x0,p,T_values): #modificado para receber os paramentros do sigma quadrado
     x_lista = []
+    x_soma_quadrado = []
+    x_soma = []
+    soma_aux = 0
+    soma_aux_quadrado = 0
     for i in range(len(T_values)):
         x_of_T = []
         for _ in range(N):
             x = Caminho_aleatorio(x0,T_values[i],p)
             x_of_T.append(x[-1])
+            soma_aux += x[-1]
+            soma_aux_quadrado += (x[-1]**2)
         x_lista.append(x_of_T)
-    return x_lista
+        x_soma.append(soma_aux)
+        x_soma_quadrado.append(soma_aux_quadrado)
+        soma_aux = 0
+        soma_aux_quadrado = 0
+    return np.array(x_lista,dtype=float), x_soma, x_soma_quadrado
 
 T_values = [5,10,100,1000,10000]
 N = 100000
-x_lista = Caminhadas_aleatorias_de_max_T(N,x0,p,T_values)
-x_lista_copy = np.array(x_lista,dtype=float)
+x_lista,x_soma,x_quadrado = Caminhadas_aleatorias_de_max_T(N,x0,p,T_values)
+#x_lista_copy = np.array(x_lista,dtype=float)
 
-print(x_lista_copy)
+#print(x_lista_copy)
+def sigma_2(x_soma,x_soma_quadrada,T_values,N):
+    Sigma_quadrado = []
+    for i in range(len(T_values)):
+        sigma = (x_soma_quadrada[i]/N) - (x_soma[i]/N)**2 
+        Sigma_quadrado.append(sigma)
+    return Sigma_quadrado
 
-def sigma_quadrado(x_list, T_values,N):
-   sigma_2 = []
-   x_2_med = 0
-   x_med = 0
-   for i in range(len(T_values)):
-       for j in range(N):
-           x_med = x_med + x_list[i][j]
-           x_2_med = x_2_med + (x_list[i][j])**2
-       sigma = x_2_med/N - (x_med/N)**2
-       sigma_2.append(sigma)
-       x_med = 0
-       x_2_med = 0
-   return sigma_2
-print(sigma_quadrado (x_lista_copy, T_values, N))
-print("___________")
+sigma_q = sigma_2[x_soma,x_quadrado,T_values,N]
+print(sigma_q)
 
+        
+
+
+#"""
+#def sigma_quadrado(x_list, T_values,N):
+#   sigma_2 = []
+#   x_2_med = 0
+#   x_med = 0
+#   for i in range(len(T_values)):
+#       for j in range(N):
+#           x_med = x_med + x_list[i][j]
+#           x_2_med = x_2_med + (x_list[i][j])**2
+#       sigma = x_2_med/N - (x_med/N)**2
+#       sigma_2.append(sigma)
+#       x_med = 0
+#       x_2_med = 0
+#   return sigma_2
+#print(sigma_quadrado (x_lista_copy, T_values, N))
+#print("___________")
+#"""
 
